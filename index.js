4f0a4096ccb67a1c2040d6e2b835437bd2c2ec49
Michele Della Mea <michele.dellamea.arcanediver@gmail.com>
2021-05-31 19:08:44 +0200

    no colors

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const morgan = require("morgan");
 const app = express();
 
 const port = process.env.PORT || 9000;
-
-app.use(morgan("dev"));
+const msg = process.env.MSG || "Hello World from Docker and Node.js!";
+app.use(morgan("common"));
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello World from Docker and Node.js!</h1>");
+  res.send(`<h1>${msg} </h1>`);
 });
 
 app.listen(port, () => {
