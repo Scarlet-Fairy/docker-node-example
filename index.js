c251687c6c5ea718d6d547eae250d074925d1142
Michele Della Mea <michele.dellamea.arcanediver@gmail.com>
2021-05-31 19:00:11 +0200

    morgan

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const express = require('express');
+const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
+
 const port = process.env.PORT || 9000;
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World from Docker and Node.js!</h1>');
+
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World from Docker and Node.js!</h1>");
 });
+
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
